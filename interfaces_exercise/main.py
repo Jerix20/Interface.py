aaf6ffea6107040d3d61b60ad1b2762976888e8e
Jerix20 <jsoto111040@umanizales.edu.co>
2024-11-06 16:40:21 -0500

    Add files via upload

---
+++ interfaces_exercise/main.py
@@ -0,0 +1,27 @@
+def main():
+    pato = Pato()
+    aguila = Aguila()
+    delfin = Delfin()
+    tiburon = Tiburon()
+    gaviota = Gaviota()
+    murcielago = Murcielago()
+
+    pato.volar()
+    pato.nadar()
+    pato.hacer_sonido()
+    pato.migrar()
+
+    aguila.volar()
+    aguila.hacer_sonido()
+
+    delfin.nadar()
+    delfin.hacer_sonido()
+
+    tiburon.nadar()
+
+    gaviota.volar()
+    gaviota.hacer_sonido()
+    gaviota.migrar()
+
+    murcielago.volar()
+    murcielago.hacer_sonido()
